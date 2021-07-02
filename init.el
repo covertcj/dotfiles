466472cb385bf03fd947993ec27a1fbf65c0a77a
Christopher J. Covert <chris.covert@blackboard.com>
2021-07-02 17:28:27 -0400

    switching to org-mode config

--- init.el
+++ init.el
@@ -1,30 +1,41 @@
-;; TODO come back and change these
-(set-face-attribute 'default nil
-		    :font "Roboto Mono"
-		    :height 160)
-
+;; Basic Window Settings ;;
 (setq inhibit-startup-message t)
+
+; remove unnecessary ui elements
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
+
+; pad the edge of the window
 (set-fringe-mode 10)
+
+; no more annoying bell noises
+(setq visible-bell nil)
+
+; keep the cursor away from the edge, and don't jump around when scrolling
 (setq scroll-margin 8)
 (setq scroll-conservatively 1)
 
-(setq visible-bell nil)
-
-(column-number-mode)
+;; Line Numbers ;;
+; relative line numbers everywhere
 (global-display-line-numbers-mode t)
+(setq display-line-numbers 'relative)
+
+; show the cursor's current column
+(column-number-mode)
+
 ; disable line numbers for certain modes
-(dolist (mode '(org-mode-hook
-		term-mode-hook
+(dolist (mode '(term-mode-hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-; make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; Fonts ;;
+(setq cjc/default-font-height 160)
+(set-face-attribute 'default nil :font "Roboto Mono" :height cjc/default-font-height)
+(set-face-attribute 'fixed-pitch nil :font "Roboto Mono" :height cjc/default-font-height)
+(set-face-attribute 'variable-pitch nil :font "Roboto" :height cjc/default-font-height)
 
-;; Package Management with use-package
+;; Setup Package Archives
 (require 'package)
 
 ; setup the package archives and make sure there is a local copy
@@ -35,34 +46,69 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-; make sure use-package is available
+;; Setup use-package ;;
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Completion
-(use-package ivy
-  :diminish
-  :bind (:map ivy-minibuffer-map
-	      ("TAB" . ivy-alt-done)
-	      ("C-j" . ivy-next-line)
-	      ("C-k" . ivy-previous-line))
-  :config
-  (ivy-mode 1))
+;; Basic Keybindings ;;
+; let ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(use-package counsel
+(use-package general
   :config
-  (counsel-mode 1))
+  (general-evil-setup)
+  (general-create-definer cjc/leader-key
+    :states 'normal
+    :prefix "SPC"
+    :non-normal-prefix "C-SPC"))
 
-(use-package ivy-rich
-  :after (ivy)
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.5))
+
+(use-package evil
   :init
-  (ivy-rich-mode 1))
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-(use-package swiper
-  :after ivy
-  :bind (("C-s" . swiper)))
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra
+  :after general)
+
+(with-eval-after-load 'general
+  (with-eval-after-load 'hydra
+
+(cjc/leader-key
+ "b"  '(:ignore b :which-key "buffers")
+ "bb" '(counsel-switch-buffer :which-key "switch buffers"))
+
+(defhydra hydra-scale-text (:timeout 6)
+  "scale editor text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(cjc/leader-key
+  "t" '(:ignore t :which-key "toggle settings")
+  "tf" '(hydra-scale-text/body :which-key "font scaling"))
+
+))
 
 (use-package all-the-icons)
 
@@ -80,56 +126,21 @@
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
 
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
+(use-package counsel
   :config
-  (setq which-key-idle-delay 0.5))
+  (counsel-mode 1))
 
-(use-package general
-  :config
-  (general-evil-setup)
-  (general-create-definer cjc/leader-key
-    :states 'normal
-    :prefix "SPC"
-    :non-normal-prefix "C-SPC")
-
-  (cjc/leader-key
-   "b"  '(:ignore b :which-key "buffers")
-   "bb" '(counsel-switch-buffer :which-key "switch buffers")))
-
-(use-package evil
+(use-package ivy-rich
+  :after (ivy)
   :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+  (ivy-rich-mode 1))
 
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line))
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper))) ; TODO: maybe move keybind
 
-(use-package evil-collection
-  :after (evil)
-  :config
-  (evil-collection-init))
-
-(use-package hydra
-  :after (general)
-  :config
-  ;; TODO: probably move this somewhere else that ends up depending on hydra
-  (defhydra hydra-scale-text (:timeout 6)
-    "scale editor text"
-    ("j" text-scale-increase "in")
-    ("k" text-scale-decrease "out")
-    ("f" nil "finished" :exit t))
-
-  ;; TODO: probably move this somewhere else that ends up depending on hydra
-  (cjc/leader-key
-    "t" '(:ignore t :which-key "toggle settings")
-    "tf" '(hydra-scale-text/body :which-key "font scaling")))
+(use-package magit)
+(use-package forge)
 
 (use-package projectile
   :after general
@@ -143,13 +154,68 @@
   (projectile-mode 1)
   (cjc/leader-key
     "p" '(:keymap projectile-command-map :wk "projectile")))
-  
-(use-package magit)
 
-;; TODO: Play with forge
-(use-package forge)
-  ;:custom
-  ;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+(use-package org
+  :hook (org-mode . cjc/org-mode-setup)
+  :config
+
+;; Basic Settings ;;
+(setq org-ellipsis " ▾"
+      org-hide-emphasis-markers t)
+
+;; Agenda Settings ;;
+(setq org-agenda-start-with-log-mode t
+      org-log-done 'time
+      org-log-into-drawer t
+      org-agenda-files
+      '("~/Google Drive/Org Notes/Tasks.org"
+        "~/Google Drive/Org Notes/Birthdays.org")
+      org-todo-keywords
+      '(("TODO(t)" "NEXT(n)" "|" "DONE(d!)")))
+
+;; Load Org Babel Languages ;;
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)))
+
+(push '("conf-unix" . conf-unix) org-src-lang-modes)
+
+;; Automatically Tangle Configuration ;;
+  (defun cjc/org-babel-tangle-config ()
+    (when (string-equal (buffer-file-name)
+                        ; TODO: rename this when the config is ready
+                        (expand-file-name "~/dev/dotfiles/emacs/init.org"))
+      (let ((org-confirm-babel-evaluate nil))
+        (org-babel-tangle))))
+  
+  (add-hook 'org-mode-hook
+            (lambda () (add-hook 'after-save-hook
+                               #'cjc/org-babel-tangle-config)))
+
+(require 'org-tempo)
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+
+(dolist (face '((org-level-1 . 1.2)
+                (org-level-2 . 1.1)
+                (org-level-3 . 1.05)
+                (org-level-4 . 1.0)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Roboto" :weight 'regular :height (cdr face)))
+
+  ; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
+  (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch)
 
 (defun cjc/org-toggle-emphasis ()
   "Toggle hiding/showing of org emphasize markers."
@@ -166,46 +232,12 @@
   (visual-line-mode 1)
   (setq evil-auto-indent nil))
 
-(use-package org
-  :hook (org-mode . cjc/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾"
-	org-hide-emphasis-markers t
+(cjc/leader-key
+  :keymaps 'org-mode-map
+  "m" '(:ignore t :which-key "org-mode")
+  "me" '(cjc/org-toggle-emphasis :which-key "toggle emphasis"))
 
-	org-agenda-start-with-log-mode t
-	org-log-done 'time
-	org-log-into-drawer t
-	org-agenda-files
-	'("~/Google Drive/Org Notes/Tasks.org"
-	  "~/Google Drive/Org Notes/Birthdays.org")
-	org-todo-keywords
-	'(("TODO(t)" "NEXT(n)" "|" "DONE(d!)"))
-
-  (cjc/leader-key
-    :keymaps 'org-mode-map
-    "m" '(:ignore t :which-key "org-mode")
-    "me" '(cjc/org-toggle-emphasis :which-key "toggle emphasis"))
-
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Roboto" :weight 'regular :height (cdr face)))
-
-    ; Ensure that anything that should be fixed-pitch in Org files appears that way
-    (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-    (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-table nil    :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
-    (set-face-attribute 'line-number nil :inherit 'fixed-pitch)
-    (set-face-attribute 'line-number-current-line nil :inherit 'fixed-pitch))
+)
 
 (use-package org-superstar
   :after org
@@ -213,39 +245,3 @@
   :config
   (setq org-superstar-headline-bullets-list '("❖" "✱" "✹" "✸" "✦" "✧")
 	org-superstar-leading-bullet " "))
-  ;(setq org-superstar-headline-bullets-list '("◉" "○" "■" "◆" "▲" "▶")))
-
-;; Replace list hyphen with dot
-(font-lock-add-keywords 'org-mode
-                        '(("^ *\\([-]\\) "
-                          (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-
-;; TODO: WIP
-;(use-package org-bullets
-;  :after org
-  
-
-;; TODO doesn't seem to do anything...
-;(use-package org-appear
-;  :hook (org-mode . org-appear-mode))
-
-; (use-package helpful
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" default))
- '(ivy-mode t)
- '(package-selected-packages
-   '(org-appear forge evil-magit magit-evil magit projectile hydra evil-collection general doom-themes ivy-rich counsel which-key use-package swiper rainbow-delimiters doom-modeline)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
