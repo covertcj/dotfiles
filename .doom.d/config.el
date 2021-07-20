3cb6f0022963a519a08412be44ed3295ad99ef4d
Christopher J. Covert <chris.covert@blackboard.com>
2021-07-20 10:20:45 -0400

    literate doom config, more migration

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,61 +1,85 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
 (setq user-full-name "Christopher J Covert"
       user-mail-address "covertops5@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-(setq doom-variable-pitch-font (font-spec :family "Roboto"      :size 14 :weight 'normal)
-      doom-font                (font-spec :family "Roboto Mono" :size 14 :weight 'normal)
-      doom-unicode-font        (font-spec :family "Roboto Mono" :size 14 :weight 'normal)
-      doom-big-font            (font-spec :family "Roboto Mono" :size 14 :weight 'normal))
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+; relative line numbers
 (setq display-line-numbers-type t
       display-line-numbers-type 'relative)
 
+; fullscreen by default
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+(defvar cjc/default-dark-theme 'doom-challenger-deep)
+(defvar cjc/default-light-theme 'doom-tomorrow-day)
+
+(setq doom-theme cjc/default-dark-theme)
+
+(setq cjc/theme-list
+  (list cjc/default-dark-theme
+        cjc/default-light-theme))
+(setq cjc/theme-index 0)
+
+
+(defun cjc/toggle-themes ()
+  "Switches between a list of themes"
+  (interactive)
+  (let* ((current-theme (nth cjc/theme-index cjc/theme-list))
+         (next-index (mod (+ cjc/theme-index 1) (length cjc/theme-list)))
+         (next-theme (nth next-index cjc/theme-list)))
+    (disable-theme current-theme)
+    (message "Theme: %s" next-theme)
+    (setq cjc/theme-index next-index)
+    (condition-case nil
+        (enable-theme next-theme)
+      (error (load-theme next-theme t)))))
+
+(map! :leader
+      :desc "toggle theme"
+      "t t" #'cjc/toggle-themes)
+
+(map! :leader
+      :desc "choose theme"
+      "t T" #'counsel-load-theme)
+
+(setq doom-variable-pitch-font (font-spec :family "Roboto"      :size 14 :weight 'normal)
+      doom-font                (font-spec :family "Roboto Mono" :size 14 :weight 'normal)
+      doom-big-font            (font-spec :family "Roboto Mono" :size 14 :weight 'normal))
+
+(setq org-directory "~/org/")
+
+(setq cjc/org-index-file (concat (file-name-as-directory    org-directory) "000 Index.org")
+      cjc/org-backlog-file (concat (file-name-as-directory  org-directory) "001 Backlog.org")
+      cjc/org-calendar-file (concat (file-name-as-directory org-directory) "002 Calendar.org")
+      cjc/org-archive-file (concat (file-name-as-directory  org-directory) "003 Archive.org")
+      cjc/org-contacts-file (concat (file-name-as-directory org-directory) "004 Contacts.org"))
+
+(after! org
+  (setq org-agenda-start-with-log-mode t
+        org-log-done 'time
+        org-log-into-drawer t
+        org-agenda-files (list cjc/org-backlog-file
+                               cjc/org-calendar-file
+                               cjc/org-archive-file
+                               cjc/org-contacts-file)
+        org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAIT(w@)" "|" "DONE(d!)" "CANCELLED(c@)")))
+
+  (setq org-refile-targets
+    '((cjc/org-archive-file :maxlevel . 1)))
+
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+
+  (require 'org-contacts)
+  (setq org-contacts-files (list cjc/org-contacts-file))
+
+  (require 'org-habit)
+  (add-to-list 'org-modules 'org-habit)
+  (setq org-habit-graph-column 60)
+
+
+  (setq org-tag-alist
+    '((:startgroup)
+      ("home" . ?h)
+      ("work" . ?w)
+      ("play" . ?p)
+      (:endgroup)
+      ("note" . ?n)
+      ("media" . ?m))))
