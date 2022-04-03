265a843fbd1968e18aaa7f419919184bd8b947a0
Christopher J. Covert <covertops5@gmail.com>
2022-04-03 14:35:42 -0400

    feat(doom): add org modern

--- doom/modules/cjc/org/config.el
+++ doom/modules/cjc/org/config.el
@@ -1,2 +1,5 @@
 (setq org-directory cjc/org-directory
       org-roam-directory cjc/org-directory)
+
+(use-package! org-modern
+  :hook (org-mode . org-modern-mode))
---
+++ doom/modules/cjc/org/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; cjc/org/packages.el
+
+(package! org-modern)
