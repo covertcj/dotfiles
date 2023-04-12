b5802eb7b8e3848066d95d8b7bc7973abc4b1c04
Christopher J. Covert <chris.covert@anthology.com>
2023-04-12 18:00:28 -0400

    feat(nvim): add community language pack plugins

--- astronvim/plugins/community.lua
+++ astronvim/plugins/community.lua
@@ -6,4 +6,11 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
 }
