dac9703975e74964d88462da7c69405d11d27bc6
Christopher J Covert <covertops5@gmail.com>
2023-04-12 21:08:59 -0400

    feat(nvim): more language support for astro

--- astronvim/plugins/community.lua
+++ astronvim/plugins/community.lua
@@ -6,11 +6,16 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typescript" },
+
+  { import = "astrocommunity.pack.bash" },
+
+  { import = "astrocommunity.pack.markdown" },
+
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
 }
--- astronvim/plugins/treesitter.lua
+++ astronvim/plugins/treesitter.lua
@@ -1,6 +1,31 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua" },
+    ensure_installed = {
+      "lua",
+      "rust",
+      "javascript",
+      "typescript",
+      "python",
+      "ruby",
+      "c",
+      "cpp",
+      "java",
+
+      "bash",
+
+      "sql",
+
+      "toml",
+      "json",
+      "json5",
+      "yaml",
+
+      "html",
+      "css",
+
+      "gitattributes",
+      "gitignore",
+    },
   },
 }
