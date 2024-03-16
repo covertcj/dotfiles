4d51f57da63c38f7e2f84d78f3dfdbb3e895f6f7
Christopher J. Covert <covertops5@gmail.com>
2024-03-16 00:15:59 -0400

    feat(kickstart-nvim): add <C-p> hotkey

--- kickstart-nvim/lua/plugins/telescope.lua
+++ kickstart-nvim/lua/plugins/telescope.lua
@@ -65,7 +65,8 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = '[F]ind [H]elp' })
     vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
-    vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Search Files' })
+    vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find Files' })
+    vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find Files' })
     vim.keymap.set('n', '<leader>ft', builtin.builtin, { desc = '[F]ind [T]elescope Command' })
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
