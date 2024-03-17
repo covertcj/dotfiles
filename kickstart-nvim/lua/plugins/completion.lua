d96d23342cfe00068c116d122ea25af141b5f1ba
Christopher J. Covert <covertops5@gmail.com>
2024-03-17 15:02:23 -0400

    feat(kickstart-nvim): add basic copilot plugin

--- kickstart-nvim/lua/plugins/completion.lua
+++ kickstart-nvim/lua/plugins/completion.lua
@@ -85,6 +85,7 @@ return { -- Autocompletion
         end, { 'i', 's' }),
       },
       sources = {
+        { name = 'copilot' },
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
