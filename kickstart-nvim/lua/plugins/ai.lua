1fea211ec77311503abf407cc503c4ff5d2f6aff
Chris Covert <chris.covert@anthology.com>
2024-03-19 17:08:38 -0400

    chore(kickstart-nvim): move copilot config to ai

--- kickstart-nvim/lua/plugins/ai.lua
+++ kickstart-nvim/lua/plugins/ai.lua
@@ -1,3 +1,6 @@
+-- TODO: investigate using inline copilot suggestions
+-- here's a good reference implementation of working with cmp
+-- https://github.com/fredrikaverpil/dotfiles/blob/main/nvim-lazyvim/lua/plugins/ai.lua
 return {
   {
     'zbirenbaum/copilot.lua',
