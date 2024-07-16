4dc445d28c50ce18e2bee8f447a512c649c81dda
Christopher J. Covert <covertops5@gmail.com>
2024-07-15 22:44:21 -0400

    feat(bash): improve aliases

--- bash/cfg/aliases.sh
+++ bash/cfg/aliases.sh
@@ -1,4 +1,17 @@
-alias gs="git status"
+alias n="nvim"
+alias o="sk_open_file"
+alias vim="nvim"
+
+alias tn="tmux new -s"
+alias ta="sk_tmux_attach_session"
+alias tw="sk_tmux_attach_window"
+alias tl="tmux ls"
+alias tp="sk_tmux_switch_pane"
+
 alias gco="git checkout"
 alias g-="git checkout -"
 alias gm-="git merge -"
+alias gs="git status"
+alias gb="sk_git_checkout_branch"
+alias gbr="sk_git_checkout_branch_remote"
+alias lg="lazygit"
