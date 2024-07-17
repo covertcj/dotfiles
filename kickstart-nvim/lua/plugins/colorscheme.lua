9e285e306511dca7ed3747471fdb44222cf891b3
Chris Covert <chris.covert@anthology.com>
2024-07-17 17:29:09 -0400

    feat(nvim): use catpuccin theme

--- kickstart-nvim/lua/plugins/colorscheme.lua
+++ kickstart-nvim/lua/plugins/colorscheme.lua
@@ -1,54 +1,15 @@
 return {
   {
-    'marko-cerovac/material.nvim',
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
     keys = {
-      { '<leader>tt', '<cmd>lua require("material.functions").change_style("deep ocean")<cr>', desc = 'Dark theme' },
-      { '<leader>tT', '<cmd>lua require("material.functions").change_style("lighter")<cr>', desc = 'Light theme' },
+      { '<leader>tt', '<CMD>colorscheme catppuccin-mocha<CR>', desc = 'Dark theme' },
+      { '<leader>tT', '<CMD>colorscheme catppuccin-latte<CR>', desc = 'Light theme' },
     },
     lazy = false,
     config = function()
-      vim.g.material_style = 'darker'
-
-      require('material').setup {
-        contrast = {
-          non_current_windows = true,
-        },
-        plugins = {
-          -- "dap",
-          -- "dashboard",
-          -- "eyeliner",
-          -- "fidget",
-          -- "flash",
-          'gitsigns',
-          'harpoon',
-          -- "hop",
-          -- "illuminate",
-          -- "indent-blankline",
-          'lspsaga',
-          'mini',
-          'neogit',
-          'neotest',
-          -- "neo-tree",
-          'neorg',
-          -- "noice",
-          'nvim-cmp',
-          -- "nvim-navic",
-          -- "nvim-tree",
-          'nvim-web-devicons',
-          -- "rainbow-delimiters",
-          -- "sneak",
-          'telescope',
-          'trouble',
-          'which-key',
-          -- "nvim-notify",
-        },
-
-        high_visibility = {
-          lighter = true,
-        },
-      }
-
-      vim.cmd.colorscheme 'material'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 }
