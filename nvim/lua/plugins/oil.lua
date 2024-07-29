ef21c10db769c1b8456a4d3cdca5b694c277717f
Chris Covert <chris.covert@anthology.com>
2024-07-29 17:35:29 -0400

    feat(nvim): oil.nvim winbar, key improvements
    Adds a winbar config that show's the current path in a bar at the top of
    oil buffers, refactors the oil config to use a more idomatic lazy.nvim
    style, and removes/overrides some keymaps that conflict with C-h and
    C-l.

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,15 +2,33 @@
 return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-
-  config = function()
-    require('oil').setup {
-      delete_to_trash = true,
-      view_options = {
-        show_hidden = true,
-      },
-    }
-
-    vim.keymap.set('n', '<leader>ff', '<cmd>Oil<cr>', { desc = 'File explorer' })
-  end,
+  opts = {
+    delete_to_trash = true,
+    view_options = {
+      show_hidden = true,
+    },
+    win_options = {
+      winbar = "%{v:lua.require('oil').get_current_dir()}",
+    },
+    -- the default keymaps bind C-h and C-l which I use to switch windows
+    use_default_keymaps = false,
+    keymaps = {
+      ['g?'] = 'actions.show_help',
+      ['<CR>'] = 'actions.select',
+      ['<C-p>'] = 'actions.preview',
+      ['<C-c>'] = 'actions.close',
+      ['<C-r>'] = 'actions.refresh',
+      ['-'] = 'actions.parent',
+      ['_'] = 'actions.open_cwd',
+      ['`'] = 'actions.cd',
+      ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' },
+      ['gs'] = 'actions.change_sort',
+      ['gx'] = 'actions.open_external',
+      ['g.'] = 'actions.toggle_hidden',
+      ['g\\'] = 'actions.toggle_trash',
+    },
+  },
+  keys = {
+    { '<leader>ff', '<cmd>Oil<cr>', desc = 'File explorer' },
+  },
 }
