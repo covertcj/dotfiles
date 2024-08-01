1c373a6dfb7f60e4e32859c5158ccb45bb9bf3a4
Chris Covert <chris.covert@anthology.com>
2024-08-01 13:37:06 -0400

    feat(nvim): one character for 'mode'

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -3,6 +3,14 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     sections = {
+      lualine_a = {
+        {
+          'mode',
+          fmt = function(str)
+            return str:sub(1, 1)
+          end,
+        },
+      },
       lualine_b = { 'diff', 'diagnostics' },
       lualine_c = {
         {
